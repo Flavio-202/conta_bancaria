acc5038de76c2c4fec754d1ea2a4f668dcb4453d
Rodrigo Aguillar <flavioaguilar2001@gmail.com>
2025-06-06 19:16:58 -0300

    ultima etapa

--- src/controller/ContaController.ts
+++ src/controller/ContaController.ts
@@ -10,7 +10,7 @@ import { colors } from "../util/Colors";
 
 export class ContaController implements ContaRepository {
 
-    private listacontas: Array <Conta> = new Array <Conta> ();
+    private listaContas: Array <Conta> = new Array <Conta> ();
     numero: number = 0;
 
     procurarPorNumero(numero: number): void {
@@ -24,14 +24,14 @@ export class ContaController implements ContaRepository {
     }
 
     listarTodas (): void {
-        for (let conta of this.listacontas){
+        for (let conta of this.listaContas){
             conta.visualizar ();
         }
 
     }
 
     cadastrar(conta: Conta): void {
-        this.listacontas.push (conta);
+        this.listaContas.push (conta);
         console.log (colors.fg.green, "\n A conta número: " + conta.numero + " foi criada com sucesso!", colors.reset);
     }
 
@@ -39,7 +39,7 @@ export class ContaController implements ContaRepository {
 
         let buscaConta = this.buscarNoArray (conta.numero);
         if (buscaConta !== null) {
-        this.listacontas [this.listacontas.indexOf (buscaConta)] = conta;
+        this.listaContas [this.listaContas.indexOf (buscaConta)] = conta;
         console.log (colors.fg.green, "\n Conta número: " + conta.numero + " foi criada com sucesso!", colors.reset);
         } else {
             console.log (`A conta número ${conta.numero} não foi encontrada`);
@@ -47,7 +47,15 @@ export class ContaController implements ContaRepository {
     }
 
     deletar(numero: number): void {
-        
+        let buscaConta = this.buscarNoArray(numero);
+
+        if (buscaConta != null) {
+            this.listaContas.splice(this.listaContas.indexOf(buscaConta), 1);
+        	console.log(colors.fg.green,"\nA Conta numero: " + numero + 
+                        " foi apagada com sucesso!", colors.reset);
+        }else
+        console.log(colors.fg.red,"\nA Conta numero: " + numero + 
+                    " não foi encontrada!", colors.reset);
     }
 
      public sacar(numero: number, valor: number): void {
@@ -99,7 +107,7 @@ export class ContaController implements ContaRepository {
     }
 
     public buscarNoArray (numero: number): Conta | null {
-        for (let conta of this.listacontas) {
+        for (let conta of this.listaContas) {
             if (conta.numero === numero)
                 return conta;
         }
